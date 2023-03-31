94ae78f5a0d5daa1ae08e002c368cfa354dae5e0
Kamil Mazelanik <kamil.mazelanik.ext@eworkgroup.com>
2023-03-31 12:23:23 +0200

    Add some content to About and Checkout page

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import { PageHero } from "../components";
 
 const CheckoutPage = () => {
-  return <main>Checkout page</main>;
+  return (
+    <main>
+      <PageHero title="checkout" />
+      <Wrapper className="page">
+        <p>Checkout page</p>
+      </Wrapper>
+    </main>
+  );
 };
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .empty {
+    text-align: center;
+  }
+`;
 
 export default CheckoutPage;
