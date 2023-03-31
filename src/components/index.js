df1f6741944074fbb7e57e453d53169f1290cac5
Kamil Mazelanik <kamil.mazelanik.ext@eworkgroup.com>
2023-03-31 20:01:52 +0200

    fetch products

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,21 @@ import PageHero from "./PageHero";
 import Hero from "./Hero";
 import Services from "./Services";
 import Contact from "./Contact";
+import Loading from "./Loading";
+import Error from "./Error";
+import FeaturedProducts from "./FeaturedProducts";
+import Product from "./Product";
 
-export { Navbar, Footer, Sidebar, PageHero, Hero, Services, Contact };
+export {
+  Navbar,
+  Footer,
+  Sidebar,
+  PageHero,
+  Hero,
+  Services,
+  Contact,
+  Loading,
+  Error,
+  FeaturedProducts,
+  Product,
+};
--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,10 +1,22 @@
-import React, { useContext, useReducer } from "react";
+import React, { useContext, useReducer, useEffect } from "react";
+import axios from "axios";
 import reducer from "../reducers/products_reducer";
+import { products_url as url } from "../utils/constants";
 
-import { SIDEBAR_OPEN, SIDEBAR_CLOSE } from "../actions";
+import {
+  SIDEBAR_OPEN,
+  SIDEBAR_CLOSE,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+} from "../actions";
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
 };
 
 const ProductsContext = React.createContext();
@@ -18,6 +30,21 @@ export const ProductsProvider = ({ children }) => {
   const closeSidebar = () => {
     dispatch({ type: SIDEBAR_CLOSE });
   };
+
+  const fetchProducts = async (url) => {
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR });
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts(url);
+  }, []);
 
   return (
     <ProductsContext.Provider
