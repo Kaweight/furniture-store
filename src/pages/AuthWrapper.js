689ca0f48746a8102fdf6bddc008ea01ae411bea
Kamil Mazelanik <kamil.mazelanik.ext@eworkgroup.com>
2023-05-06 14:02:07 +0200

    Updates

--- src/pages/AuthWrapper.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function AuthWrapper() {
-  return <p> AuthWrapper </p>;
-}
-
-export default AuthWrapper;
--- src/pages/PrivateRoute.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-
-const PrivateRoute = ({ children }) => {
-  return <p>Private Route</p>;
-};
-export default PrivateRoute;
